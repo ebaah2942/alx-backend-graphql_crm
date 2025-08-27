3e562d076e5d10e2f68d35605226dea0d931f8f6
Enoch Baah <ebaah2942@gmail.com>
2025-08-27 00:09:55 +0000

    Added another files

--- crm/settings.py
+++ crm/settings.py
@@ -1,3 +1,17 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'crm',
+    'graphal_crm',
+    'graphene_django',
+    'django_filters',
+    'django_crontab',
+]
+
 CRONJOBS = [
     ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
 ]
